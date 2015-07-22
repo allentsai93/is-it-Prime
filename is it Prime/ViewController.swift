a85167b7072a1c3a0de651a46c3b79f6ace073bb
Allen Tsai <allentsai93@gmail.com>
2015-07-21 22:52:19 -0700

    checks if number inputted in app is a prime number

--- is it Prime/ViewController.swift
+++ is it Prime/ViewController.swift
@@ -10,6 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var resultLabel: UILabel!
+    @IBOutlet var numberInput: UITextField!
+    @IBAction func primeButton(sender: AnyObject) {
+        var number = numberInput.text.toInt()
+        
+        var isPrime = true
+        
+        if number == 1 || number == 2 {
+            isPrime = false
+            resultLabel.text = "\(numberInput.text) is not prime!"
+        }
+        
+        if number != 2 && number != 1{
+            for var i = 2; i < number; i++ {
+                if number! % i == 0 {
+                    isPrime = false
+                    resultLabel.text = "\(numberInput.text) is not prime!"
+                } else if isPrime == true {
+                    resultLabel.text = "\(numberInput.text) is prime!"
+            }
+            
+        }
+        }
+        
+        println(isPrime)
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
